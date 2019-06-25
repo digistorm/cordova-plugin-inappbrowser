2b01d3b6cc5f76feff6536c91d8947e91ecca7f9
Richard Gilbert <richard.gilbert@digistorm.com.au>
2019-06-25 16:30:47 +1000

    Handle SSL errors on Android

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1299,6 +1299,51 @@ public class InAppBrowser extends CordovaPlugin {
             this.waitForBeforeload = beforeload != null;
         }
 
+        // Rejected by Google Play - Failed to validate the certificate chain
+        @Override
+        public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(cordova.getActivity());
+            String message = "SSL Certificate error.";
+            switch (error.getPrimaryError()) {
+                case SslError.SSL_NOTYETVALID:
+                    message = "The certificate is not yet valid.";
+                    break;
+                case SslError.SSL_EXPIRED:
+                    message = "The certificate has expired.";
+                    break;
+                case SslError.SSL_IDMISMATCH:
+                    message = "Hostname mismatch.";
+                    break;
+                case SslError.SSL_UNTRUSTED:
+                    message = "The certificate authority is not trusted.";
+                    break;
+                case SslError.SSL_DATE_INVALID:
+                    message = "The date of the certificate is invalid.";
+                    break;
+                case SslError.SSL_INVALID:
+                    message = "A generic error occurred.";
+                    break;
+            }
+            message += " Do you want to continue anyway?";
+
+            builder.setTitle("SSL Certificate Error");
+            builder.setMessage(message);
+            builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    handler.proceed();
+                }
+            });
+            builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    handler.cancel();
+                }
+            });
+            final AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+
         /**
          * Override the URL that should be loaded
          *
