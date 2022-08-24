09c52a83a17881adfa51f4a1bf04f66d4f795212
Nathan Westerlaken <nathan.westerlaken@digistorm.com>
2022-08-24 10:59:02 +1000

    fix android ssl handle

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1260,51 +1260,6 @@ public class InAppBrowser extends CordovaPlugin {
             this.waitForBeforeload = beforeload != null;
         }
 
-        // Prompt user to allow failed SSL
-        @Override
-        public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
-            final AlertDialog.Builder builder = new AlertDialog.Builder(cordova.getActivity());
-            String message = "SSL Certificate error.";
-            switch (error.getPrimaryError()) {
-                case SslError.SSL_NOTYETVALID:
-                    message = "The certificate is not yet valid.";
-                    break;
-                case SslError.SSL_EXPIRED:
-                    message = "The certificate has expired.";
-                    break;
-                case SslError.SSL_IDMISMATCH:
-                    message = "Hostname mismatch.";
-                    break;
-                case SslError.SSL_UNTRUSTED:
-                    message = "The certificate authority is not trusted.";
-                    break;
-                case SslError.SSL_DATE_INVALID:
-                    message = "The date of the certificate is invalid.";
-                    break;
-                case SslError.SSL_INVALID:
-                    message = "A generic error occurred.";
-                    break;
-            }
-            message += " Do you want to continue anyway?";
-
-            builder.setTitle("SSL Certificate Error");
-            builder.setMessage(message);
-            builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    handler.proceed();
-                }
-            });
-            builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    handler.cancel();
-                }
-            });
-            final AlertDialog dialog = builder.create();
-            dialog.show();
-        }
-
         /**
          * Override the URL that should be loaded
          *
@@ -1573,44 +1528,49 @@ public class InAppBrowser extends CordovaPlugin {
             }
         }
 
+        // Prompt user to allow failed SSL
         @Override
-        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-            super.onReceivedSslError(view, handler, error);
-            try {
-                JSONObject obj = new JSONObject();
-                obj.put("type", LOAD_ERROR_EVENT);
-                obj.put("url", error.getUrl());
-                obj.put("code", 0);
-                obj.put("sslerror", error.getPrimaryError());
-                String message;
-                switch (error.getPrimaryError()) {
-                case SslError.SSL_DATE_INVALID:
-                    message = "The date of the certificate is invalid";
+        public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(cordova.getActivity());
+            String message = "SSL Certificate error.";
+            switch (error.getPrimaryError()) {
+                case SslError.SSL_NOTYETVALID:
+                    message = "The certificate is not yet valid.";
                     break;
                 case SslError.SSL_EXPIRED:
-                    message = "The certificate has expired";
+                    message = "The certificate has expired.";
                     break;
                 case SslError.SSL_IDMISMATCH:
-                    message = "Hostname mismatch";
-                    break;
-                default:
-                case SslError.SSL_INVALID:
-                    message = "A generic error occurred";
-                    break;
-                case SslError.SSL_NOTYETVALID:
-                    message = "The certificate is not yet valid";
+                    message = "Hostname mismatch.";
                     break;
                 case SslError.SSL_UNTRUSTED:
-                    message = "The certificate authority is not trusted";
+                    message = "The certificate authority is not trusted.";
                     break;
-                }
-                obj.put("message", message);
-
-                sendUpdate(obj, true, PluginResult.Status.ERROR);
-            } catch (JSONException ex) {
-                LOG.d(LOG_TAG, "Should never happen");
+                case SslError.SSL_DATE_INVALID:
+                    message = "The date of the certificate is invalid.";
+                    break;
+                case SslError.SSL_INVALID:
+                    message = "A generic error occurred.";
+                    break;
             }
-            handler.cancel();
+            message += " Do you want to continue anyway?";
+
+            builder.setTitle("SSL Certificate Error");
+            builder.setMessage(message);
+            builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    handler.proceed();
+                }
+            });
+            builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    handler.cancel();
+                }
+            });
+            final AlertDialog dialog = builder.create();
+            dialog.show();
         }
 
         /**
